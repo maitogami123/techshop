087b435726fa0f4c07c9e1618aa42dc047172bd8
Linh <Linh@gmail.com>
2023-05-03 18:49:31 +0700

    Update Register

--- public/js/validator.js
+++ public/js/validator.js
@@ -113,6 +113,7 @@ Validator.isEmail = function (selector) {
             }else {
                 let regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
                 if (!regex.test(value)) {
+<<<<<<< HEAD
 
                     return "Email vừa nhập không đúng. Vui lòng nhập lại!"
                 }
@@ -121,6 +122,12 @@ Validator.isEmail = function (selector) {
                         return "Email này đã tồn tại. Vui lòng nhập email khác!"
                     }                    
                 }
+=======
+                    return "Email vừa nhập không đúng. Vui lòng nhập lại!"
+                }else if (option.Gmail == value){
+                    return "Email này đã tồn tại. Vui lòng nhập email khác!"
+                }
+>>>>>>> c69c1ac (Update Register)
             }   
             return undefined
         }
