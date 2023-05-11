99fc46385d335c66c5313fbbe70f28cd7f8fb378
Shikigami Maito <51812674+maitogami123@users.noreply.github.com>
2023-05-12 04:24:35 +0700

    Delete .history directory

--- .history/app/Controllers/BrandController_20230511112042.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controllers;
-
-use App\Models\Brands;
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\RouteCollection;
-?>
--- .history/views/admin/brand/create.view_20230511110011.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (!isLoggedIn() || !in_array('P_Create', $user->getPermissions()))
-  redirect($routes->get('homepage')->getPath())
-    ?>
